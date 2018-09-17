54c2e11e10d0c813fd8a84306bbb349a4de035ac
Kumo Tyou <zy.hua1122@qq.com>
2018-09-17 22:58:22 +0800

    store.d.ts

--- types/store.d.ts
+++ types/store.d.ts
@@ -1,7 +1,7 @@
 // import { RouterStore as _RouterStore } from 'mobx-react-router';
 
 declare interface IStore {
-  userStore: IUserStore.UserStore
-  globalStore: IGlobalStore.GlobalStore
+  userStore: IUserStore.UserStore;
+  globalStore: IGlobalStore.GlobalStore;
   // routerStore: RouterStore;
 }
