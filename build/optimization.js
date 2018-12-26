6b64153454a02c08405e768a777b320844a7d8d5
Zhan9YunHua <zy.hua1122@qq.com>
2018-12-26 18:12:47 +0800

    dev

--- build/optimization.js
+++ build/optimization.js
@@ -24,7 +24,19 @@ module.exports =
           new UglifyJsPlugin({
             cache: true,
             parallel: true,
-            sourceMap: config.sourceMap
+            sourceMap: config.sourceMap,
+            extractComments: false,
+            uglifyOptions: {
+              warnings: false,
+              parse: {},
+              compress: {},
+              mangle: true, // Note `mangle.properties` is `false` by default.
+              output: null,
+              toplevel: false,
+              nameCache: null,
+              ie8: false,
+              keep_fnames: false
+            }
           }),
           new OptimizeCSSAssetsPlugin({
             cssProcessor: require('cssnano'),
